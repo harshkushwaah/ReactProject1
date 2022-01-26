57602954002ed9a2b201288bde2ea985ffb88e3e
Cherry <harshkingkushwah@gmail.com>
2022-01-26 19:18:41 +0530

    Version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Restaurant from './components/basics/Restaurant';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <>
+  <Restaurant />
+  
+  </>;
+  
+};
+
+const Harsh = () => {
+  return <h1>Harsh component</h1>
 }
 
-export default App;
+export default App; 
